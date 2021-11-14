85e32e613bd4ee34ce0beeec7d9d9223f5250720
MiguelAvil <maavilab@unah.hn>
2021-11-14 01:31:35 -0600

    Creacion de button edit y delete con sus funciones .js

--- Articulos.js
+++ Articulos.js
@@ -1,6 +1,8 @@
 var UrlGetArticulos = 'http://localhost:80/G4_19/controller/articulos.php?op=GetArticulos';
 var UrlPostArticulo = 'http://localhost:80/G4_19/controller/articulos.php?op=InsertArticulos';
-
+var UrlGetUno = 'http://localhost:80/G4_19/controller/articulos.php?op=GetUno';
+var UrlPutArticulo = 'http://localhost:80/G4_19/controller/articulos.php?op=UpdateArticulos';
+var UrlDaleteArticulo = 'http://localhost:80/G4_19/controller/articulos.php?op=DeleteArticulos';
 // cuando el documento(pagina este lista) aparecen los datos de articulos
 $(document).ready(function(){
     CargarArticulos();
@@ -25,7 +27,11 @@ function CargarArticulos(){
                 '<td>'+ MiItems[i].PORCENTAJE_ISV +'</td>'+
                 '<td>'+ MiItems[i].ESTADO +'</td>'+
                 '<td>'+ MiItems[i].ID_SOCIO +'</td>'+
-            '</tr>';
+                '<td>'+
+                '<button class="btn btn-outline-info" onclick="CargarArticulo('+MiItems[i].ID+')">Editar</button>'+
+                '<button class="btn btn-outline-danger" onclick="EliminarArticulo('+MiItems[i].ID+')">Eliminar</button>'+
+                '</td>'+
+                '</tr>';
             $('.articulos').html(Valores);
             }
         }
@@ -56,4 +62,79 @@ function AgregarArticulo(){
         }
     });
     alert("Articulo Agredado");
+}
+
+function CargarArticulo(idarticulo){
+    var datosarticulo = {
+        id: idarticulo
+    };
+    var datosarticulojson = JSON.stringify(datosarticulo);
+    // alert(datosarticulojson);
+    $.ajax({        
+        url: UrlGetUno,
+        type: 'POST',
+        data: datosarticulojson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            var MiItems = response;
+            $('#descripcion').val(MiItems[0].DESCRIPCION);
+            $('#unidad').val(MiItems[0].UNIDAD);
+            $('#costo').val(MiItems[0].COSTO);
+            $('#precio').val(MiItems[0].PRECIO);
+            $('#aplica_isv').val(MiItems[0].APLICA_ISV);
+            $('#porcentaje_isv').val(MiItems[0].PORCENTAJE_ISV);
+            $('#id_socio').val(MiItems[0].ID_SOCIO);
+            var btnactualizar = '<input type="submit" id="btn_actualizar" onclick="ActualizarArticulo('+MiItems[0].ID+')"'+
+            'value="Actualizar Articulo" class="btn btn-outline-primary"><input>';
+            $('.button').html(btnactualizar);
+        }
+    });
+}
+
+function ActualizarArticulo(idarticulo){
+    var datosarticulo = {
+        id: idarticulo,
+        descripcion: $('#descripcion').val(),
+        unidad: $('#unidad').val(),
+        costo: $('#costo').val(),
+        precio: $('#precio').val(),
+        aplica_isv: $('#aplica_isv').val(),
+        porcentaje_isv: $('#porcentaje_isv').val(),
+        id_socio: $('#id_socio').val()
+    };
+    var datosarticulojson = JSON.stringify(datosarticulo);
+
+    $.ajax({
+        url: UrlPutArticulo,
+        type: 'PUT',
+        data: datosarticulojson,
+        datatype: 'JSON',
+        contentType: 'aplication/json',
+        success: function(response){
+            console.log(response);
+            
+        }
+    });
+    alert("Articulo Actualizado");
+}
+
+function EliminarArticulo(idarticulo){
+    var datosarticulo = {
+        id: idarticulo
+    };
+    var datosarticulojson = JSON.stringify(datosarticulo);
+
+    $.ajax({
+        url: UrlDaleteArticulo,
+        type: 'DELETE',
+        data: datosarticulojson,
+        datatype: 'JSON',
+        contentType: 'aplication/json',
+        success: function(response){
+            console.log(response);
+            
+        }
+    });
+    alert("Articulo Eliminado");
 }
